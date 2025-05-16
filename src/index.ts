fb36ed636516ada66729d13e7376ad412cbd4f3a
Nicola Migone <nicolamigone179@gmail.com>
2025-05-16 18:08:13 +0200

    Clean up remove email

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 export * from "./dao/ddb/transcriptions";
-export * from "./dao/ddb/emails";
-export * from "./dao/ses";
 export * from "./dao/s3/audio";
 export * from "./dao/clerk/users";
 export * from "./models/transcription";
--- src/models/emails.ts
+++
@@ -1,3 +0,0 @@
-export type IdempotencyItem = {
-  id: string;
-};
