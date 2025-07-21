bd517c7493f3e6bd1e7410400aae041f369fa1d7
Nicola Migone <nicolamigone179@gmail.com>
2025-07-21 09:54:15 +0200

    Export subscriptions model

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * from "./dao/ddb/subscriptions";
 export * from "./dao/s3/audio";
 export * from "./dao/clerk/users";
 export * from "./models/transcription";
+export * from "./models/subscription";
+export * from "./models/user";
